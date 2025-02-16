1ef001989c1b2f7aa6092609f9f988e2917d775b
Martín M <skydiver@users.noreply.github.com>
2025-02-16 00:10:33 -0300

    fix build scripts

--- scripts/pack.ts
+++ scripts/pack.ts
@@ -2,16 +2,16 @@ import archiver from 'archiver';
 import { createWriteStream, existsSync, mkdirSync } from 'fs';
 import path from 'path';
 
-const buildDir = `${process.cwd()}/build`;
+const distDir = `${process.cwd()}/dist`;
 const workflowFile = `${path.basename(process.cwd())}.alfredworkflow`;
 
-/* Ensure build directory exists */
-if (!existsSync(buildDir)) {
-  mkdirSync(buildDir, { recursive: true }); // Create directory if missing
+/* Ensure dist directory exists */
+if (!existsSync(distDir)) {
+  mkdirSync(distDir, { recursive: true }); // Create directory if missing
 }
 
 /* Create a new zip file */
-const output = createWriteStream(`${buildDir}/${workflowFile}`);
+const output = createWriteStream(`${distDir}/${workflowFile}`);
 const archive = archiver('zip', { zlib: { level: 9 } });
 
 output.on('close', () => console.log(`Created ${workflowFile} (${archive.pointer()} bytes)`));
@@ -23,7 +23,7 @@ archive.pipe(output);
 /* Add files to the archive */
 archive.file('assets/icon.png', { name: 'icon.png' });
 archive.file('assets/info.plist', { name: 'info.plist' });
-archive.file('dist/bundle.js', { name: 'bundle.js' });
+archive.file('build/bundle.js', { name: 'bundle.js' });
 
 /* Finalize the archive */
 archive.finalize();
