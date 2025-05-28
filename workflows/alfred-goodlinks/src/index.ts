a3db01c93fdb18dce9a3746cef103973b49383a0
Martín M <skydiver@users.noreply.github.com>
2025-05-28 20:06:28 -0300

    created basic workflow

--- workflows/alfred-goodlinks/src/index.ts
+++ workflows/alfred-goodlinks/src/index.ts
@@ -1,3 +1,33 @@
+import minimist from 'minimist';
+
 (async () => {
-  console.log('Hello from Alfred Goodlinks!');
+  const args = minimist(process.argv.slice(2), { '--': true });
+
+  const urls = args['--']?.length ? args['--'] : args._;
+
+  let data: { title: string; subtitle: string; arg: string }[];
+
+  if (!urls || urls.length === 0) {
+    data = [
+      {
+        title: 'No URLs provided',
+        subtitle: 'Cannot retrieve URLs from the specified browser',
+        arg: '',
+      },
+    ];
+  } else {
+    data = urls.map((url: string) => {
+      return {
+        title: url,
+        subtitle: `Save ${url} to GoodLinks`,
+        arg: url,
+      };
+    });
+  }
+
+  const result = {
+    items: data,
+  };
+
+  console.log(JSON.stringify(result, null, 2));
 })();
